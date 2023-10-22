49b87909c8702f6cdfa4bf608b9fa9c4fa0da952
GAURAV SHARMA <109570190+gauravs7673@users.noreply.github.com>
2023-10-22 20:22:48 +0530

    Add files via upload

---
+++ Contact.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+const Contact = () => {
+  return (
+    <div className="section">
+      <h2>Contact Me</h2>
+      <p>Vit Email: gaurav.2021@vitstudetn.ac.in</p>
+      <p>Personal Email: gauravs7376@gmail.com</p>
+      <p>Phone: 6386261296</p>
+      <p>LinkedIn:https://www.linkedin.com/in/gaurav-sharma-2bbb66244/</p>
+    </div>
+  );
+};
+
+export default Contact;
